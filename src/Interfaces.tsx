2cfa0d5bf8cc06975bbfcfa9fa7845281d7089f9
Matthias Jänicke <morphbreed@chello.at>
2021-03-28 01:21:54 +0100

    improve types

--- src/Interfaces.tsx
+++ src/Interfaces.tsx
@@ -1,10 +1,27 @@
 export interface IApartment {
-    url: string;
-    size: number;
-    rooms: number;
-    rent: number;
-    deposit: number;
-    address: string;
-    source: string;
+  url: string;
+  size: number;
+  rooms: number;
+  rent: number;
+  deposit: number;
+  address: string;
+  source: string;
+}
 
+export type IApartmentProperty =
+  | 'url'
+  | 'size'
+  | 'rooms'
+  | 'rent'
+  | 'deposit'
+  | 'address'
+  | 'source';
+
+export type ISortArray = [IApartment, number];
+
+export interface ITableHeaderData {
+  name: string;
+  property: string;
+  sortable?: boolean;
+  align?: 'left' | 'center' | 'right' | 'justify' | 'inherit' | undefined;
 }
